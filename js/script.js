3d2e5e51a9bb5440992b5d145efd267e8ccc2962
PaulPourtout <paulpourtout@hotmail.com>
2016-11-20 01:40:55 +0100

    rectification JS + animation css

--- js/script.js
+++ js/script.js
@@ -2,24 +2,22 @@ $(document).ready(function() {
 
     var lastLocation = 0;
     $(window).scroll(function() {
-
-
         var currentLocation = $(this).scrollTop();
-        $("#scroll-score").html(currentLocation);
-        if (currentLocation > lastLocation) {
-            $("#main-nav").animate({
-                top: -70
-            }, 500);
-            //   $("#main-nav").slideUp();
+        // $("#scroll-score").html(currentLocation);
+        if (currentLocation > 70) {
+            if (currentLocation > lastLocation) {
+                $("#main-nav").stop().animate({
+                    top: -70
+                }, 'fast');
+                //   $("#main-nav").slideUp();
 
-        } else {
-
-            $("#main-nav").animate({
-                top: 0
-            }, 500);
-            //   $("#main-nav").slideDown();
-
+            } else {
+                $("#main-nav").stop().animate({
+                    top: 0
+                }, 'fast');
+                //   $("#main-nav").slideDown();
+            }
+            lastLocation = currentLocation;
         }
-        lastLocation = currentLocation;
     });
 });
